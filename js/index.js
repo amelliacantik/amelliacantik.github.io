cce986749fc37b8564f94d1900c89d74ef47c506
amelliacantik <rzaardian69@gmail.com>
2024-11-21 15:37:29 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector(".title");
-const text = "Haii Amel Ini ada Hadiah Dari Reza".split("");
+const text = "Haii Amel Ini ada Hadiah".split("");
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== " ") {
     title.innerHTML += `<span>${text[index]}<span/>`;
